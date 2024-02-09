9686d233a2ffa3ebc701c8d4b613c3e0cff9312f
Spadavecchia-LM <leonardo.spadavecchia@hotmail.com>
2024-02-08 21:21:45 -0300

    primer commit

---
+++ src/main/java/com/Equipo2/RaceACar/RaceACarApplication.java
@@ -0,0 +1,13 @@
+package com.Equipo2.RaceACar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceACarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceACarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Equipo2/RaceACar/RaceACarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Equipo2.RaceACar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
