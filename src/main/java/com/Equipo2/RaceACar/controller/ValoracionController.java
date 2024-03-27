b0e893c823a2e97fc6149e161e72b1b8a361b56c
Brian Mermel <bmermel@gmail.com>
2024-03-26 23:08:43 -0300

    controlador de valoraciones finalizado

--- src/main/java/com/Equipo2/RaceACar/controller/ValoracionController.java
+++ src/main/java/com/Equipo2/RaceACar/controller/ValoracionController.java
@@ -1,4 +1,64 @@
 package com.Equipo2.RaceACar.controller;
 
+import com.Equipo2.RaceACar.DTO.ValoracionDTO;
+import com.Equipo2.RaceACar.model.Valoracion;
+import com.Equipo2.RaceACar.repository.ValoracionRepository;
+import com.Equipo2.RaceACar.service.ValoracionService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/valoraciones")
 public class ValoracionController {
+
+    @Autowired
+    private ValoracionRepository valoracionRepository;
+
+    @Autowired
+    private ValoracionService service;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @GetMapping("/{idAuto}")
+    public List<ValoracionDTO> obtenerValoracionesPorIdAuto(@PathVariable Long idAuto) {
+        List<Valoracion> valoraciones = valoracionRepository.findByReservaAutoId(idAuto);
+
+        if (valoraciones.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return valoraciones.stream()
+                .map(valoracion -> modelMapper.map(valoracion, ValoracionDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{autoId}/promedio")
+    public Double obtenerPromedioValoracionPorAutoId(@PathVariable Long autoId) {
+        Double promedio = valoracionRepository.obtenerPromedioValoracionPorAutoId(autoId);
+        return service.redondearADosDecimales(promedio);
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<?> crearValoracion(
+            @RequestParam Long reservaId,
+            @RequestParam Integer valoracion,
+            @RequestParam String resena
+    ) {
+        try {
+            ValoracionDTO nuevaValoracion = service.crearValoracion(reservaId, valoracion, resena);
+            return ResponseEntity.status(HttpStatus.CREATED).body(nuevaValoracion);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró la reserva con el ID proporcionado.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocurrió un error al crear la valoración.");
+        }
+    }
 }
--- src/main/java/com/Equipo2/RaceACar/service/ValoracionService.java
+++ src/main/java/com/Equipo2/RaceACar/service/ValoracionService.java
@@ -10,7 +10,10 @@ import com.Equipo2.RaceACar.repository.ValoracionRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.text.DecimalFormat;
 import java.time.LocalDate;
@@ -28,6 +31,7 @@ public class ValoracionService {
     @Autowired
     private ValoracionRepository valoracionRepository;
 
+
     public ValoracionDTO crearValoracion(Long reservaId, Integer valoracion, String resena) {
         Reserva reserva = reservaRepository.findById(reservaId)
                 .orElseThrow(() -> new NoSuchElementException("No se encontró la reserva con ID: " + reservaId));
@@ -41,7 +45,8 @@ public class ValoracionService {
         Valoracion valoracionGuardada = valoracionRepository.save(nuevaValoracion);
         return modelMapper.map(valoracionGuardada, ValoracionDTO.class);
     }
-    public List<ValoracionDTO> obtenerTodasLasValoraciones() {
+/* metodo no utilizado ver si borrar
+*     public List<ValoracionDTO> obtenerTodasLasValoraciones() {
         List<Valoracion> valoraciones = valoracionRepository.findAll();
 
         if (valoraciones.isEmpty()) {
@@ -52,6 +57,7 @@ public class ValoracionService {
                 .map(valoracion -> modelMapper.map(valoracion, ValoracionDTO.class))
                 .collect(Collectors.toList());
     }
+* */
 
     public List<ValoracionDTO> obtenerValoracionesPorIdAuto(Long idAuto) {
         List<Valoracion> valoraciones = valoracionRepository.findByReservaAutoId(idAuto);
